fc33728b2e6233a8bb5d38fbc793d216cbf5bce7
Victor Prokhorov <mail@victorprokhorov.com>
2025-02-08 16:21:09 +0100

    hardcode one answer and stream reponse

---
+++ ollama-api/src/main.rs
@@ -0,0 +1,19 @@
+use ollama_rs::Ollama;
+use ollama_rs::generation::completion::request::GenerationRequest;
+use tokio::io::stdout;
+use tokio::io::AsyncWriteExt;
+use tokio_stream::StreamExt;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+	let ollama = Ollama::default();
+	let mut stdout = stdout();
+	let mut stream = ollama.generate_stream(GenerationRequest::new("alice:latest".to_string(), "why is the sky blue?".to_string())).await?;
+	while let Some(Ok(responses)) = stream.next().await {
+		for response in responses {
+			stdout.write_all(response.response.as_bytes()).await?;
+			stdout.flush().await?;
+		}
+	}
+	Ok(())
+}
